874266e1461d67f22a6315e4861bb32568aac61a
Grazy0range <76399929+Grazy0range@users.noreply.github.com>
2021-06-01 18:11:15 +0300

    1234

--- send.php
+++ send.php
@@ -7,9 +7,9 @@ $name = urldecode($name);
 $phone = urldecode($phone);
 $name = trim($name);
 $phone = trim($phone);
-echo $name;
-echo "<br>";
-echo $phone;
+// echo $name;
+// echo "<br>";
+// echo $phone;
 if (mail("gabbaman35@gmail.com", "Заявка с сайта", "Имя:".$name.". Телефон: ".$phone ,"From: example2@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
